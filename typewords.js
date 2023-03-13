58dec6df7335bcd337ca1443aec8ba6a835849bb
Fr13nds202201 <pythonhack22@gmail.com>
2023-03-12 23:43:32 -0500

    responsive

--- typewords.js
+++ typewords.js
@@ -1,3 +1,18 @@
+//############ toggle icon narvar  #################
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+navbar.classList.remove('active');
+menuIcon.classList.remove('bx-x');
+
+
+//############ escritura typed  #################
+
 var type = new Typed(".multiple-text",{
     strings:["Frontend Developer", "Autodidact","Creative"],
     typeSpeed:100,
@@ -5,3 +20,40 @@ var type = new Typed(".multiple-text",{
     backDelay:1000,
     loop:true
 })
+var type1 = new Typed(".heading", {
+    strings:["My `<span>Skills</span>`"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+var type2 = new Typed(".project", {
+    strings:["Lates `<span>Projects</span>`"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+var type3 = new Typed(".contact__me", {
+    strings:["Contact `<span>me !</span>`"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+
+
+
+//############ Scroll Section Active link#################
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header nav a');
+
+// window.onscroll=()=>{
+//     sections.array.forEach(element => {
+        
+//     });
+// }
+
+//############  remove toggle icon and navbar when click link #################
+
+
